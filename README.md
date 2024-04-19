95d6a5f542c89e77dc54ce277ed4bae0bcc6a262
RudychikFedorV <rudychik.fedor@gmail.com>
2024-04-19 20:08:56 +0300

    section best, swiper

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+#Gulp, html, scss, js, swiper, doc.json, mixins, variables.
 [Modimal-gulp](https://rudychikfedorv.github.io/Modimal-gulp/)
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,40 @@
 // import header from './modules/header.js';
 // header();
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 2,
+	spaceBetween: 16,
+    parallax: true,
+    speed: 1000,
+    keyboard: {
+      enabled: true,
+    },
+	
+
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 24,
+		},
+		420: {
+			slidesPerView: 1.8,
+			spaceBetween: 24,
+		},
+		576: {
+			slidesPerView: 2.2,
+			spaceBetween: 24,
+		},
+		768: {
+			slidesPerView: 3,
+			spaceBetween: 24,
+		}
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
